7d98df9fe93112caca1866fe65edba7d022e1126
Milosz <miloszbaranski@outlook.com>
2022-04-26 00:20:50 +0200

    commit

---
+++ src/elements/Navbar/CurrencySwitcher.tsx
@@ -0,0 +1,21 @@
+import StyledSelect from "elements/layout/StyledSelect";
+
+interface Props {
+  updateCurrency: (arg0: string) => void;
+}
+
+const CurrencySwitcher: React.FC<Props> = ({ updateCurrency }) => {
+  const currencies: string[] = ["USD", "EUR", "PLN"];
+
+  return (
+    <StyledSelect onChange={(e) => updateCurrency(e.target.value)}>
+      {currencies.map((currency) => (
+        <option key={currency} value={currency}>
+          {currency}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+};
+
+export default CurrencySwitcher;
