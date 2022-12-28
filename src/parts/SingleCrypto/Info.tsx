d2cf542ba3776d5303e1fe9d77cf19bbcacd8387
Milosz <miloszbaranski@outlook.com>
2022-12-29 00:57:02 +0100

    use enum, use interface, move styled component to the bottom of file, add conditional display, change props

--- src/parts/SingleCrypto/Info.tsx
+++ src/parts/SingleCrypto/Info.tsx
@@ -1,41 +1,47 @@
 import styled from "styled-components";
+
 import Identification from "elements/SingleCrypto/Identification";
 import Links from "elements/SingleCrypto/Links";
 import Price from "elements/SingleCrypto/Price";
 
-interface Props {
-  cryptoDetails: {
-    symbol?: string;
-    name?: string;
-    icon?: string;
-    twitterUrl?: string;
-    websiteUrl?: string;
-    price?: number;
-  };
-  currency: string;
+import { ECurrencySymbol } from "enums/ECurrencySymbol";
+import { ICryptoDetails } from "interfaces/ICryptoDetails";
+
+interface IProps {
+  cryptoDetails: ICryptoDetails;
+  currencySymbol: ECurrencySymbol;
 }
+
+const Info: React.FC<IProps> = ({ cryptoDetails, currencySymbol }) => {
+  return (
+    <>
+      {cryptoDetails && (
+        <>
+          <StyledInfo>
+            <Identification
+              symbol={cryptoDetails.symbol}
+              name={cryptoDetails.name}
+              icon={cryptoDetails.icon}
+            />
+            <Links
+              twitter={cryptoDetails.twitterUrl}
+              website={cryptoDetails.websiteUrl}
+            />
+            <Price
+              price={cryptoDetails.price}
+              currencySymbol={currencySymbol}
+            />
+          </StyledInfo>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Info;
 
 const StyledInfo = styled.div`
   padding: 35px 0;
   display: flex;
   flex-wrap: wrap;
 `;
-
-const Info: React.FC<Props> = ({ cryptoDetails, currency }) => {
-  return (
-    <StyledInfo>
-      <Identification
-        symbol={cryptoDetails.symbol}
-        name={cryptoDetails.name}
-        icon={cryptoDetails.icon}
-      />
-      <Links
-        twitter={cryptoDetails.twitterUrl}
-        website={cryptoDetails.websiteUrl}
-      />
-      <Price price={cryptoDetails.price} currency={currency} />
-    </StyledInfo>
-  );
-};
-
-export default Info;
