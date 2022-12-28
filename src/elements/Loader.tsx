cf59b546e38304e7e27b2d910c176f80b34df681
Milosz <miloszbaranski@outlook.com>
2022-12-29 00:49:14 +0100

    move styled component to the bottom of file, use theme interface

--- src/elements/Loader.tsx
+++ src/elements/Loader.tsx
@@ -1,17 +1,10 @@
 import { RotatingLines } from "react-loader-spinner";
 import styled, { useTheme } from "styled-components";
 
-const StyledLoader = styled.div`
-  width: 100%;
-  flex-basis: 100%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding-top: 35px;
-`;
+import { ITheme } from "interfaces/ITheme";
 
 const Loader: React.FC = () => {
-  const theme: any = useTheme();
+  const theme = useTheme() as ITheme;
   return (
     <StyledLoader>
       <RotatingLines width="60" strokeColor={theme.colors.main} />
@@ -20,3 +13,12 @@ const Loader: React.FC = () => {
 };
 
 export default Loader;
+
+const StyledLoader = styled.div`
+  width: 100%;
+  flex-basis: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 40px 0;
+`;
