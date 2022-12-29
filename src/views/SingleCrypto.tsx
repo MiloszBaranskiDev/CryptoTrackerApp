885210830bf173e5811e1ff2f8540671ecdc512a
Milosz <miloszbaranski@outlook.com>
2022-12-29 01:01:14 +0100

    add API error, move styled component to the bottom of file, use enums, use interface, update import path

--- src/views/SingleCrypto.tsx
+++ src/views/SingleCrypto.tsx
@@ -1,42 +1,67 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import StyledWrapper from "elements/layout/StyledWrapper";
-import GetSingleCryptoDetails from "utils/GetSingleCryptoDetails";
+
+import ApiError from "elements/ApiError";
+import StyledWrapper from "elements/styled/StyledWrapper";
 import Info from "parts/SingleCrypto/Info";
 import PeriodSwitcher from "parts/SingleCrypto/PeriodSwitcher";
 import Chart from "parts/SingleCrypto/Chart";
 import Loader from "elements/Loader";
 
-interface Props {
-  currency: string;
+import { ECurrencySymbol } from "enums/ECurrencySymbol";
+import { EPeriod } from "enums/EPeriod";
+import { ICryptoDetails } from "interfaces/ICryptoDetails";
+
+import GetSingleCryptoDetails from "api/GetSingleCryptoDetails";
+
+interface IProps {
+  currencySymbol: ECurrencySymbol;
 }
 
-const SingleCrypto: React.FC<Props> = ({ currency }) => {
+const SingleCrypto: React.FC<IProps> = ({ currencySymbol }) => {
   const { id } = useParams() as { id: string };
-  const [cryptoDetails, setCryptoDetails] = useState<object>({});
-  const [period, updatePeriod] = useState<string>("24h");
+  const [cryptoDetails, setCryptoDetails] = useState<ICryptoDetails>();
+  const [period, updatePeriod] = useState<EPeriod>(EPeriod.day);
   const [loading, setLoading] = useState<boolean>(true);
+  const [showError, setShowError] = useState<boolean>(false);
+
+  const loadCryptoDetails = async () => {
+    setLoading(true);
+    const loadedDetails = await GetSingleCryptoDetails(id!, currencySymbol);
+    if (loadedDetails === undefined || loadedDetails === null) {
+      setShowError(true);
+    } else {
+      setCryptoDetails({
+        symbol: loadedDetails.symbol,
+        name: loadedDetails.name,
+        icon: loadedDetails.icon,
+        twitterUrl: loadedDetails.twitterUrl,
+        websiteUrl: loadedDetails.websiteUrl,
+        price: loadedDetails.price,
+      });
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    setLoading(true);
-    const loadCryptoDetails = async () => {
-      setCryptoDetails(await GetSingleCryptoDetails(id!, currency));
-      setLoading(false);
-    };
     loadCryptoDetails();
-  }, [id, currency]);
+    // eslint-disable-next-line
+  }, [id, currencySymbol]);
 
   return (
     <StyledWrapper>
-      {!loading ? (
+      {loading && <Loader />}
+      {!loading && showError ? <ApiError /> : null}
+      {!loading && !showError ? (
         <>
-          <Info cryptoDetails={cryptoDetails} currency={currency} />
+          <Info
+            cryptoDetails={cryptoDetails!}
+            currencySymbol={currencySymbol}
+          />
           <PeriodSwitcher currentPeriod={period} updatePeriod={updatePeriod} />
-          <Chart currency={currency} id={id} period={period} />
+          <Chart currencySymbol={currencySymbol} id={id} period={period} />
         </>
-      ) : (
-        <Loader />
-      )}
+      ) : null}
     </StyledWrapper>
   );
 };
