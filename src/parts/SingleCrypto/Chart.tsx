c54f5d085e906ab6c6d21f095e4f82b236e8d10b
Milosz <miloszbaranski@outlook.com>
2022-12-29 01:00:02 +0100

    update imports paths, use enum, use interface, use new function, rename interface, move styled component to the bottom of file, add API error

--- src/parts/SingleCrypto/Chart.tsx
+++ src/parts/SingleCrypto/Chart.tsx
@@ -8,32 +8,37 @@ import {
   Tooltip,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import GetSingleCryptoHistory from "utils/GetSingleCryptoHistory";
-import GetSingleCryptoDetails from "utils/GetSingleCryptoDetails";
-import GetPriceFraction from "utils/GetPriceFraction";
 import styled, { useTheme } from "styled-components";
 
-interface Props {
-  currency: string;
+import GetSingleCryptoHistory from "api/GetSingleCryptoHistory";
+import GetSingleCryptoDetails from "api/GetSingleCryptoDetails";
+
+import { EPeriod } from "enums/EPeriod";
+import { ECurrencySymbol } from "enums/ECurrencySymbol";
+import { ITheme } from "interfaces/ITheme";
+
+import GetNumberToPlainString from "utils/GetNumberToPlainString";
+
+import Loader from "elements/Loader";
+import ApiError from "elements/ApiError";
+
+interface IProps {
+  currencySymbol: ECurrencySymbol;
   id: string;
-  period: string;
+  period: EPeriod;
 }
 
-interface ChartData {
+interface IChartData {
   labels: number[];
   datasets: [
     {
       borderWidth: number;
-      borderColor: any;
-      backgroundColor: any;
+      borderColor: string;
+      backgroundColor: string;
       data: number[];
     }
   ];
 }
-
-const StyledChart = styled.div`
-  padding: 25px 0 160px 0;
-`;
 
 ChartJS.register(
   CategoryScale,
@@ -43,69 +48,78 @@ ChartJS.register(
   Tooltip
 );
 
-const Chart: React.FC<Props> = ({ currency, id, period }) => {
+const options: object = {
+  responsive: true,
+  scales: {
+    x: {
+      display: false,
+    },
+  },
+  elements: {
+    point: {
+      radius: 1.65,
+    },
+  },
+};
+
+const Chart: React.FC<IProps> = ({ currencySymbol, id, period }) => {
+  const theme = useTheme() as ITheme;
   const [history, setHistory] = useState<number[]>();
   const [usdInCurrentCurrency, setUsdToCurrentCurrency] = useState<number>();
-  const [data, setData] = useState<ChartData>(null as any);
-  const theme: any = useTheme();
-  const historicalPrices: number[] = [];
+  const [data, setData] = useState<IChartData>(null as any);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [showError, setShowError] = useState<boolean>(false);
 
-  const options: object = {
-    responsive: true,
-    scales: {
-      x: {
-        display: false,
-      },
-    },
-    elements: {
-      point: {
-        radius: 1.65,
-      },
-    },
+  const historicalPrices: string[] = [];
+
+  const loadCryptoHistory: VoidFunction = async () => {
+    setLoading(true);
+    const loadedCryptoHistory = await GetSingleCryptoHistory(id, period);
+    if (loadedCryptoHistory === undefined || loadedCryptoHistory === null) {
+      setShowError(true);
+    } else {
+      setHistory(loadedCryptoHistory.map((obj: number[]) => obj[1]));
+    }
+    setLoading(false);
   };
 
   useEffect(() => {
     new Promise<{ price: number }>((resolve) => {
-      resolve(GetSingleCryptoDetails("tether", currency));
+      resolve(GetSingleCryptoDetails("tether", currencySymbol));
     }).then((r) => setUsdToCurrentCurrency(r.price));
-  }, [currency]);
+  }, [currencySymbol]);
 
-  // to fix error about memory leak
+  // fix error about memory leak
+  // eslint-disable-next-line
   const [didMount, setDidMount] = useState<boolean>(false);
-  //
   useEffect(() => {
     setDidMount(true);
     if (id !== undefined) {
-      const loadCryptoHistory: VoidFunction = async () => {
-        setHistory(await GetSingleCryptoHistory(id, period));
-      };
       loadCryptoHistory();
     }
     return () => setDidMount(false);
-  }, [id, period, currency]);
+    // eslint-disable-next-line
+  }, [id, period, currencySymbol]);
 
   useEffect(() => {
     if (history) {
       historicalPrices.length = 0;
-      history.forEach((item: any) => {
-        const historicalPrice: number = Number(GetPriceFraction(item[1]));
-        if (currency === "USD") {
-          historicalPrices.push(historicalPrice);
-        } else {
-          const fixedHistoricalPrice: number =
-            historicalPrice * usdInCurrentCurrency!;
-          historicalPrices.push(Number(GetPriceFraction(fixedHistoricalPrice)));
-        }
+      history.forEach((price: number) => {
+        historicalPrices.push(
+          currencySymbol === ECurrencySymbol.usd
+            ? GetNumberToPlainString(price)
+            : GetNumberToPlainString(price * usdInCurrentCurrency!)
+        );
       });
 
       setData({
-        labels: historicalPrices,
+        labels: historicalPrices as any,
         datasets: [
           {
             borderWidth: 1,
             borderColor: theme.colors.main,
             backgroundColor: theme.colors.main,
-            data: historicalPrices,
+            data: historicalPrices as any,
           },
         ],
       });
@@ -115,9 +129,17 @@ const Chart: React.FC<Props> = ({ currency, id, period }) => {
 
   return (
     <StyledChart>
-      {data !== null && <Line options={options} data={data} />}
+      {loading && <Loader />}
+      {!loading && showError ? <ApiError /> : null}
+      {!loading && !showError ? (
+        <>{data !== null && <Line options={options} data={data} />}</>
+      ) : null}
     </StyledChart>
   );
 };
 
 export default Chart;
+
+const StyledChart = styled.div`
+  padding: 25px 0 160px 0;
+`;
